34cc614beca985f9bb3060f56b752e18c1c2930e
ahmadraza7761 <127116532+ahmadraza7761@users.noreply.github.com>
2023-07-29 04:14:03 -0700

    Add files via upload

---
+++ dom assignment/script.js
@@ -0,0 +1,31 @@
+// Q no 1
+// How we can access HTML elements using DOM in javascript?
+Ans:
+// We can access html elements by following methods
+// 1-By Id
+let element1 = document.getElementById("elementId");
+// 2-by class
+let element2 = document.getElementsByClassName("className");
+// 3 by tag 
+let element3 = document.getElementsByTagName("tagName");
+
+// 4 by query 
+let element4 = document.querySelector("CSS selector");
+
+
+// Q no 2
+// What are the activities that we can perform using DOM?
+// Using the Document Object Model (DOM) in JavaScript, we can perform various activities on HTML element:
+// 1.We can access and modify html elements in javascript to perform various events and function on the webpage.
+
+// 2. Adding and Removing Elements: You can dynamically create new HTML elements and insert them into the DOM using different methods
+
+// Q no 3
+//  What are the event-listners in DOM?
+//Event listeners are used to handle and respond to various events triggered by user interactions or other actions on HTML elements. Event listeners are JavaScript functions that are attached to specific elements and executes when event occurs
+// Some event listners are following
+// 1-click 
+// 2-submit
+// 3-Change 
+// 4-Load 
+// 5-Resize 
